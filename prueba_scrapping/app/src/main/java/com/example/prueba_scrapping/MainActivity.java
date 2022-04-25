105624de4445b4bd55e2c9bfe00dfe0e515e6e0a
Miguel18529 <amigovillar@gmail.com>
2022-04-25 21:13:47 +0200

    ?

--- prueba_scrapping/app/src/main/java/com/example/prueba_scrapping/MainActivity.java
+++ prueba_scrapping/app/src/main/java/com/example/prueba_scrapping/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.prueba_scrapping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,22 +21,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void verWeb(View view){
-        try{
-            Document doc = Jsoup.connect("https://www.formula1.com/en.html").get();
-            System.out.println("Buscando en el html..." + doc.title());
-            Elements content = doc.getElementsByTag("body");
-            System.out.println(content.size());
-            for(int i = 0; i < content.size(); i++){
-                System.out.println(content.get(i).toString());
-            }
 
-            System.out.println("Finalizado");
-        } catch(Exception e){
-            System.out.println(e);
+
+    Thread thread = new Thread(new Runnable() {
+        private TextView tv;
+        @Override
+        public void run() {
+            try  {
+                tv = (TextView) findViewById(R.id.textView);
+                Document doc = Jsoup.connect("https://www.formula1.com/en/drivers.html").get();
+                System.out.println("Buscando en el html..." + doc.title());
+                Elements content = doc.getElementsByClass("f1-black--xxl no-margin");
+                String c = content.text();
+                //TODO: Error al usar el tv, parece que al hilo no le gusta usar cosas externas
+                tv.setText(c);
+            } catch (Exception e) {
+                System.out.println("Esto es muy poco guayando por tu parte:( " + e);
+            }
         }
+    });
+
+
+
+    public void verWeb(View view){
+
+        thread.start();
+
 
     }
 }
