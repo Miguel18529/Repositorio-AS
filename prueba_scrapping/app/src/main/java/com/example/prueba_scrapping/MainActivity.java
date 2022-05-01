a4d6fc0b9df53201fc96622291f1dc409dd9c59c
Miguel18529 <48023764+Miguel18529@users.noreply.github.com>
2022-05-01 10:51:18 +0200

    Jsoup

--- prueba_scrapping/app/src/main/java/com/example/prueba_scrapping/MainActivity.java
+++ prueba_scrapping/app/src/main/java/com/example/prueba_scrapping/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -17,38 +18,62 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        boton = findViewById(R.id.button);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verWeb();
+            }
+        });
 
     }
 
 
+    public void verWeb(){
+        new Thread(new Runnable() {
 
-    Thread thread = new Thread(new Runnable() {
-        private TextView tv;
-        @Override
-        public void run() {
-            try  {
-                tv = (TextView) findViewById(R.id.textView);
-                Document doc = Jsoup.connect("https://www.formula1.com/en/drivers.html").get();
-                System.out.println("Buscando en el html..." + doc.title());
-                Elements content = doc.getElementsByClass("f1-black--xxl no-margin");
-                String c = content.text();
-                //TODO: Error al usar el tv, parece que al hilo no le gusta usar cosas externas
-                tv.setText(c);
-            } catch (Exception e) {
-                System.out.println("Esto es muy poco guayando por tu parte:( " + e);
+            @Override
+            public void run() {
+                final StringBuilder c = new StringBuilder();
+
+                try  {
+                    String url = "https://www.formula1.com";
+                    Document doc = Jsoup.connect(url + "/en/drivers.html").get();
+                    System.out.println("Buscando en el html..." + doc.title());
+                    Elements content = doc.getElementsByClass("f1-black--xxl no-margin");
+                    c.append(content.text());
+
+                    Elements links = doc.select("a[href]");
+                    System.out.println(links.size());
+                    for (int i = 0; i < links.size(); i++){
+                        String link = links.get(i).getElementsByClass("listing-item--link ").attr("href");
+                        Document doc_aux = Jsoup.connect(url + link).get();
+                        System.out.println(doc_aux.getElementsByClass("stat-list").text());
+
+                    }
+
+                    System.out.println("Finalizado");
+
+                } catch (Exception e) {
+                    System.out.println("Esto es muy poco guayando por tu parte:( " + e);
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(c.toString());
+                    }
+                });
             }
-        }
-    });
-
-
-
-    public void verWeb(View view){
-
-        thread.start();
+        }).start();
 
 
     }
